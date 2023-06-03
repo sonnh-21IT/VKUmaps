43e630233894a8b232aae837ceaf83b7f108c368
SON NGUYEN HONG <sonnh.21it@gmail.com>
2023-06-04 04:06:51 +0700

    Code comit

--- app/src/main/java/com/example/vkumaps/MainActivity.java
+++ app/src/main/java/com/example/vkumaps/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.vkumaps;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.example.vkumaps.fragment.AdmissionsFragment;
 import com.example.vkumaps.fragment.EventFragment;
@@ -23,54 +24,55 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ChangeFragmentListener {
     private DrawerLayout drawerLayout;
-    private static final int FRAGMENT_HOME=0;
-    private static final int FRAGMENT_ADMISSION=1;
-    private static final int FRAGMENT_EVENT=2;
-    private int currentFragment=FRAGMENT_HOME;
-
+    private static final int FRAGMENT_HOME = 0;
+    private static final int FRAGMENT_ADMISSION = 1;
+    private static final int FRAGMENT_EVENT = 2;
+    private int currentFragment = FRAGMENT_HOME;
     private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SplashScreen.installSplashScreen(this);
-
+        int nightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+        AppCompatDelegate.setDefaultNightMode(nightMode);
         setContentView(R.layout.activity_main);
 
-        toolbar=findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.nav_drawer_open,R.string.nav_drawer_close);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView=findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         replaceFragment(new HomeFragment(this));
-        currentFragment=FRAGMENT_HOME;
+        currentFragment = FRAGMENT_HOME;
         navigationView.getMenu().findItem(R.id.nav_maps).setChecked(true);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
-        if(id==R.id.nav_maps){
-            if (currentFragment!=FRAGMENT_HOME){
+        int id = item.getItemId();
+        if (id == R.id.nav_maps) {
+            if (currentFragment != FRAGMENT_HOME) {
                 replaceFragment(new HomeFragment(this));
-                currentFragment=FRAGMENT_HOME;
+                currentFragment = FRAGMENT_HOME;
                 drawerLayout.closeDrawer(GravityCompat.START);
             }
-        }else if(id==R.id.nav_admissions){
-            if (currentFragment!=FRAGMENT_ADMISSION){
+        } else if (id == R.id.nav_admissions) {
+            if (currentFragment != FRAGMENT_ADMISSION) {
                 replaceFragment(new AdmissionsFragment(this));
-                currentFragment=FRAGMENT_ADMISSION;
+                currentFragment = FRAGMENT_ADMISSION;
                 drawerLayout.closeDrawer(GravityCompat.START);
             }
-        }else if(id==R.id.nav_event){
-            if (currentFragment!=FRAGMENT_EVENT){
+        } else if (id == R.id.nav_event) {
+            if (currentFragment != FRAGMENT_EVENT) {
                 replaceFragment(new EventFragment(this));
-                currentFragment=FRAGMENT_EVENT;
+                currentFragment = FRAGMENT_EVENT;
                 drawerLayout.closeDrawer(GravityCompat.START);
             }
         }
@@ -79,20 +81,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else{
-            if(HomeFragment.currentstate==0){
+        } else {
+            if (HomeFragment.currentstate == 0) {
                 super.onBackPressed();
-            }else{
+            } else {
                 HomeFragment.bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                HomeFragment.currentstate=0;
+                HomeFragment.currentstate = 0;
             }
         }
     }
-    private void replaceFragment(Fragment fragment){
-        FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame,fragment);
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, fragment);
         transaction.commit();
     }
 
