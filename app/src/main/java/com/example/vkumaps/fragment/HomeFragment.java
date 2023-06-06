f8db7594fbe46a6b4247e22e432463f64aefe383
SON NGUYEN HONG <sonnh.21it@gmail.com>
2023-06-06 11:35:29 +0700

    nav commit

--- app/src/main/java/com/example/vkumaps/fragment/HomeFragment.java
+++ app/src/main/java/com/example/vkumaps/fragment/HomeFragment.java
@@ -1,44 +1,38 @@
 package com.example.vkumaps.fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.SeekBar;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.vkumaps.MainActivity;
 import com.example.vkumaps.R;
 import com.example.vkumaps.listener.ChangeFragmentListener;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -47,10 +41,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-import java.util.Map;
 
-
-public class HomeFragment extends Fragment implements OnMapReadyCallback{
+public class HomeFragment extends Fragment implements OnMapReadyCallback,ActivityCompat.OnRequestPermissionsResultCallback{
     private static final int REQUEST_PERMISSION_CODE = 10;
     //    private SupportMapFragment mapFragment;
     private ChangeFragmentListener listener;
@@ -60,7 +52,14 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
     private FrameLayout sheet;
     private static final String TAG = "PERMISSION_TAG";
     private GoogleMap map;
+    private SeekBar customDurationBar;
+    private PolylineOptions currPolylineOptions;
+    private boolean isCanceled = false;
+
     private LocationManager locationManager;
+
+    public static final LatLng VKU_LOCATION = new LatLng(15.9754993744594, 108.25236572354167);
+    ;
 
     public HomeFragment(ChangeFragmentListener listener) {
         this.listener = listener;
@@ -74,7 +73,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         oc = rootView.findViewById(R.id.btn);
         sheet = rootView.findViewById(R.id.sheet);
         SupportMapFragment mapFragment = SupportMapFragment.newInstance();
-        onRequestPermission();
+        requestPermission();
         getActivity().getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.map_view, mapFragment)
@@ -88,57 +87,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        map.clear();
-        //set vị trí
-        LatLng vku_location = new LatLng(15.9754993744594, 108.25236572354167);
-        //set kiểu map
-        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-        //thêm điểm đánh dấu
-        map.addMarker(new MarkerOptions()
-                .position(vku_location)
-                .title("Trường Đại học Công nghệ Thông tin và Truyên thông Viêt-Hàn")
-                .snippet("Trường Đại học công lập đào tạo sâu về công nghệ thông tin và kinh tế số duy nhất tại miền trung")
-                .icon(bitmapDescriptorFromVector(getActivity().getApplicationContext(), R.drawable.ic_favorite)));
-        //Di chuyển máy ảnh đến tọa độ bản đồ và phóng to gần hơn.
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(vku_location));
-//        googleMap.moveCamera(CameraUpdateFactory.zoomTo(17));
-        map.animateCamera(CameraUpdateFactory.zoomIn());
-        map.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
-        // Lấy đối tượng UiSettings từ GoogleMap
-        UiSettings uiSettings = map.getUiSettings();
-        map.getUiSettings().setIndoorLevelPickerEnabled(true);
-        //
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(vku_location)      // Sets the center of the map to Mountain View
-                .zoom(17)                   // Sets the zoom
-                .bearing(270)                // Sets the orientation of the camera to east
-                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-                .build();                   // Creates a CameraPosition from the builder
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-        // Tắt thanh công cụ bản đồ
-        uiSettings.setMapToolbarEnabled(false);
-        if (!(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-            map.setMyLocationEnabled(true);
-        }
-        LatLng lc2 = new LatLng(15.977280636378287, 108.25767284270289);
-        map.addPolyline(new PolylineOptions().add(
-                vku_location,
-                new LatLng(15.97654583271555, 108.25463967304078),
-                new LatLng(15.977938586228822, 108.25490284089005),
-                lc2
-        ).width(10).color(Color.RED));
-        //hiển thị maps
-        map.setTrafficEnabled(true);
+        mapSetup();
+        uiSettings();
     }
 
-    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorId) {
-        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorId);
-        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
-        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        vectorDrawable.draw(canvas);
-        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        requestPermission();
     }
 
     @Override
@@ -166,12 +121,54 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         });
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        onRequestPermission();
+    private void uiSettings() {
+        map.getUiSettings().setIndoorLevelPickerEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setMapToolbarEnabled(false);
     }
 
-    private void onRequestPermission() {
+    private void mapSetup() {
+        map.clear();
+        map.setPadding(0, 0, 0, 550);
+        cameraSetup();
+        markerSetup();
+        //vị trí hiện tại
+        if (!(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            map.setMyLocationEnabled(true);
+        }
+        //hiển thị maps
+        map.setTrafficEnabled(true);
+    }
+
+    private void cameraSetup() {
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(VKU_LOCATION)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(270)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        map.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    private void markerSetup() {
+        map.addMarker(new MarkerOptions()
+                .position(VKU_LOCATION)
+                .title("Trường Đại học Công nghệ Thông tin và Truyên thông Viêt-Hàn")
+                .snippet("Trường Đại học công lập đào tạo sâu về công nghệ thông tin và kinh tế số duy nhất tại miền trung"));
+    }
+
+
+    private BitmapDescriptor bitmapDescriptorFromVectorForMarker(Context context, int vectorId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    private void requestPermission() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             return;
         }
