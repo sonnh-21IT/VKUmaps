179ca471cc6793cf03d6dc088938de3440acc8a6
Tran Xuan Tien <tranxuantienvtqs@gmail.com>
2023-06-09 09:58:08 +0700

    update login and feedback

--- app/src/main/java/com/example/vkumaps/activities/LoginActivity.java
+++ app/src/main/java/com/example/vkumaps/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -33,6 +34,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Toolbar toolbarLogin = findViewById(R.id.toolbarLogin);
+
+        toolbarLogin.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         // Xóa thông tin đăng nhập lưu trữ của phiên trước
         GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN).signOut();
