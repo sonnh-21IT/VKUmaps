8501a4f6c5729d507b4b824175752e0d29258006
SON NGUYEN HONG <sonnh.21it@gmail.com>
2023-06-09 14:21:47 +0700

    search fragment

---
+++ app/src/main/java/com/example/vkumaps/adapters/NestedAdapter.java
@@ -0,0 +1,47 @@
+package com.example.vkumaps.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vkumaps.R;
+
+import java.util.List;
+
+public class NestedAdapter extends RecyclerView.Adapter<NestedAdapter.MyViewHolder> {
+    private List<String> mList;
+
+    public NestedAdapter(List<String> mList) {
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.nested_item , parent , false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.mTv.setText(mList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView mTv;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTv = itemView.findViewById(R.id.nestedItemTv);
+        }
+    }
+}
--- app/src/main/java/com/example/vkumaps/fragment/EventFragment.java
+++ app/src/main/java/com/example/vkumaps/fragment/EventFragment.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,13 +13,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vkumaps.R;
 import com.example.vkumaps.activities.BrowserActivity;
-import com.example.vkumaps.activities.MainActivity;
 import com.example.vkumaps.adapters.NewsAdapter;
 import com.example.vkumaps.listener.ChangeFragmentListener;
 import com.example.vkumaps.listener.ItemNewsClickListener;
 import com.example.vkumaps.models.News;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -37,14 +33,15 @@ public class EventFragment extends Fragment implements ItemNewsClickListener {
     private NewsAdapter adapter;
     private ChangeFragmentListener listener;
     private ConstraintLayout loadView;
-    public EventFragment(ChangeFragmentListener listener){
-        this.listener=listener;
+
+    public EventFragment(ChangeFragmentListener listener) {
+        this.listener = listener;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView=inflater.inflate(R.layout.fragment_event, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_event, container, false);
         // Inflate the layout for this fragment
         listener.changeTitle("Sự kiện");
         loadView = rootView.findViewById(R.id.loader_view);
@@ -52,18 +49,18 @@ public class EventFragment extends Fragment implements ItemNewsClickListener {
         rc.setVisibility(View.GONE);
         loadView.setVisibility(View.VISIBLE);
         rc.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
         rc.setLayoutManager(layoutManager);
         firestore = FirebaseFirestore.getInstance();
         listNews = new ArrayList<>();
-        adapter=new NewsAdapter(listNews,this);
+        adapter = new NewsAdapter(listNews, this);
         rc.setAdapter(adapter);
 
         firestore.collection("newstable").orderBy("created", Query.Direction.DESCENDING)
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
+                    public void onComplete(Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 News newsModel = document.toObject(News.class);
                                 listNews.add(newsModel);
@@ -71,8 +68,6 @@ public class EventFragment extends Fragment implements ItemNewsClickListener {
                                 loadView.setVisibility(View.GONE);
                                 rc.setVisibility(View.VISIBLE);
                             }
-                        } else {
-                            Toast.makeText(getContext(), task.getException() + "", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
@@ -82,9 +77,9 @@ public class EventFragment extends Fragment implements ItemNewsClickListener {
 
     @Override
     public void onItemClick(News news) {
-        Intent intent=new Intent(requireContext(),BrowserActivity.class);
-        intent.putExtra("url",news.getUrl());
-        intent.putExtra("title",news.getTitle());
+        Intent intent = new Intent(requireContext(), BrowserActivity.class);
+        intent.putExtra("url", news.getUrl());
+        intent.putExtra("title", news.getTitle());
         startActivity(intent);
     }
 }
