03c32b674a338f64f32d9ddad3547812827bdae6
SON NGUYEN HONG <sonnh.21it@gmail.com>
2023-06-20 16:41:38 +0700

    commit miscellaneous

--- app/src/main/java/com/example/vkumaps/fragment/HomeFragment.java
+++ app/src/main/java/com/example/vkumaps/fragment/HomeFragment.java
@@ -1,11 +1,13 @@
 package com.example.vkumaps.fragment;
 
 import android.Manifest;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -26,6 +28,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.vkumaps.R;
 import com.example.vkumaps.listener.ChangeFragmentListener;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -36,25 +39,32 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.maps.android.data.Feature;
+import com.google.maps.android.data.Geometry;
 import com.google.maps.android.data.kml.KmlLayer;
 import com.google.maps.android.data.kml.KmlPlacemark;
+import com.google.maps.android.data.kml.KmlPoint;
+import com.google.maps.android.data.kml.KmlPolygon;
 
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.util.List;
 
 
-public class HomeFragment extends Fragment implements OnMapReadyCallback {
+public class HomeFragment extends Fragment implements OnMapReadyCallback{
     private ChangeFragmentListener listener;
     public static int currentstate;
     public static BottomSheetBehavior<View> bottomSheetBehavior;
-    private ImageView oc,zoomIn,zoomOut;
+    private ImageView oc, zoomIn, zoomOut;
     private FrameLayout sheet;
     private static final String TAG = "PERMISSION_TAG";
     private GoogleMap map;
     private LocationManager locationManager;
+    private KmlLayer kmlLayer;
     public static final LatLng VKU_LOCATION = new LatLng(15.9754993744594, 108.25236572354167);
     private ActivityResultLauncher<String> resultLauncher;
 
@@ -68,8 +78,8 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         oc = rootView.findViewById(R.id.btn);
-        zoomOut=rootView.findViewById(R.id.zoom_out);
-        zoomIn=rootView.findViewById(R.id.zoom_in);
+        zoomOut = rootView.findViewById(R.id.zoom_out);
+        zoomIn = rootView.findViewById(R.id.zoom_in);
         sheet = rootView.findViewById(R.id.sheet);
         SupportMapFragment mapFragment = SupportMapFragment.newInstance();
         requestPermission();
@@ -81,21 +91,14 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         locationManager = (LocationManager) getActivity().getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
 
         resultLauncher = registerForActivityResult(
-            new ActivityResultContracts.RequestPermission(),
-            isGranted -> {
-                if (isGranted) {
-                    if (!(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-                        map.setMyLocationEnabled(true);
-                        map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
-                            @Override
-                            public boolean onMyLocationButtonClick() {
-
-                                return false;
-                            }
-                        });
+                new ActivityResultContracts.RequestPermission(),
+                isGranted -> {
+                    if (isGranted) {
+                        if (!(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+                            map.setMyLocationEnabled(true);
+                        }
                     }
                 }
-            }
         );
         return rootView;
     }
@@ -103,8 +106,45 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+//                                    LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                if (!(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+                    Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 20);
+//                    map.animateCamera(cameraUpdate);
+                    cameraSetup(latLng, 20, 0);
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    currentstate = 0;
+                }
+                return true;
+            }
+        });
         try {
             mapSetup();
+            kmlLayer.setOnFeatureClickListener(new KmlLayer.OnFeatureClickListener() {
+                @Override
+                public void onFeatureClick(Feature feature) {
+                    Geometry geometry = feature.getGeometry();
+                    if (geometry != null && geometry.getGeometryType().equals("Polygon")) {
+                        // Bỏ qua sự kiện click trên đa giác (polygon)
+                        return;
+                    } else if (geometry.getGeometryType().equals("Point")) {
+                        KmlPoint kmlPoint = (KmlPoint) geometry;
+                        LatLng pointCoordinates = kmlPoint.getGeometryObject();
+                        LatLngBounds bounds = new LatLngBounds(
+                                new LatLng(15.975398, 108.251979), // Tọa độ góc tây nam của hình chữ nhật
+                                new LatLng(15.975481, 108.252580)  // Tọa độ góc đông bắc của hình chữ nhật
+                        );
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                        currentstate = 1;
+//                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pointCoordinates,20));
+                        cameraSetup(pointCoordinates,20,0);
+                    }
+                }
+            });
         } catch (XmlPullParserException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -161,8 +201,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void mapSetup() throws XmlPullParserException, IOException {
-        map.clear();
-        cameraSetup();
+        cameraSetup(VKU_LOCATION, 16.5f, 30);
         //vị trí hiện tại
         if (!(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
             map.setMyLocationEnabled(true);
@@ -170,8 +209,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         //hiển thị maps
         map.setTrafficEnabled(true);
 
+
         //Vẽ các khu vực lên maps
-        KmlLayer kmlLayer = new KmlLayer(map, R.raw.vkustudent, getContext());
+        kmlLayer = new KmlLayer(map, R.raw.vkustudent, getContext());
         kmlLayer.addLayerToMap();
 
         if (kmlLayer.isLayerOnMap()) {
@@ -183,19 +223,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
             Toast.makeText(getContext(), "error", Toast.LENGTH_SHORT).show();
         }
 
-        kmlLayer.setOnFeatureClickListener(new KmlLayer.OnFeatureClickListener() {
-            @Override
-            public void onFeatureClick(Feature feature) {
-                String name = feature.getProperty("name");
-                if (name != null) {
-                    // Hiển thị tên khu vực
-                    Toast.makeText(getContext(), name + "", Toast.LENGTH_SHORT).show();
-                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                    currentstate = 1;
-                }
-            }
-        });
-
         map.setMaxZoomPreference(23);
         map.setMinZoomPreference(16.5f);
         LatLngBounds bounds = new LatLngBounds(
@@ -204,16 +231,16 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         );
         map.setLatLngBoundsForCameraTarget(bounds);
         map.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.mymapstyle));
+
     }
 
-    private void cameraSetup() {
+    private void cameraSetup(LatLng latLng, float zoom, int tilt) {
         CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(VKU_LOCATION)      // Sets the center of the map to Mountain View
-                .zoom(16.5f)                   // Sets the zoom
-                .bearing(270)                // Sets the orientation of the camera to east
-                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .target(latLng)      // Sets the center of the map to Mountain View
+                .zoom(zoom)                   // Sets the zoom
+                .tilt(tilt)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
-        map.animateCamera(CameraUpdateFactory.zoomTo(16.5f), 1000, null);
+        map.animateCamera(CameraUpdateFactory.zoomTo(zoom), 1000, null);
         map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
